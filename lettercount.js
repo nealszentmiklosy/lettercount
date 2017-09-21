bb8d54075b4441578e5c96fe906926c65a0eacc5
nealszentmiklosy <nszent@gmail.com>
2017-09-21 09:45:11 -0400

    solved

--- lettercount.js
+++ lettercount.js
@@ -1,17 +1,11 @@
 //count number of letters in a given string
-//string to be checked
-// function (string,check,count){
-// 	for(k=0;k<count.length;k++){
-// 		if(check==string[k])
-// 	}
-// }
 let string = "alexander"
 var letters = {}
 for (i=0;i<string.length;i++){
-	if(letters[string[i]]==false){
-		letters[string[i]] = string[i]
+	if(letters[string[i]]==undefined){
+		letters[string[i]] = 1
 	} else {
 		letters[string[i]]++
 	}
-}
+};
 console.log(letters);
