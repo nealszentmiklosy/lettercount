5e80fa05265df24ff4a6e00e914bcd0ef041e6d5
nealszentmiklosy <nszent@gmail.com>
2017-09-21 09:42:20 -0400

    making progress

--- lettercount.js
+++ lettercount.js
@@ -1,6 +1,17 @@
 //count number of letters in a given string
 //string to be checked
+// function (string,check,count){
+// 	for(k=0;k<count.length;k++){
+// 		if(check==string[k])
+// 	}
+// }
 let string = "alexander"
+var letters = {}
 for (i=0;i<string.length;i++){
-	console.log (string.charAt(i))
+	if(letters[string[i]]==false){
+		letters[string[i]] = string[i]
+	} else {
+		letters[string[i]]++
+	}
 }
+console.log(letters);
